a2397360800c0eadd57b03a679976b8e8967f4a6
AmAcreep <sorngmini46@gmail.com>
2025-11-05 15:26:10 +0700

    Update: sample default data, and added function

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,20 @@
-let cart = [];
+export let cart = [{
+    productid: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 20
+}, {
+    productid: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 10
+}
+];
+
+export function addToCart(productId) {
+    const existingProduct = cart.find(cartItem => cartItem.productid === productId);
+    if(existingProduct) {
+        existingProduct.quantity += 1;
+    } else { 
+        cart.push({
+            productid: productId,
+            quantity: 1
+        });
+    }
+}
